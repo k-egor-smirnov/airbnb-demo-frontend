fb7e10f3da53ec02b35f449044bf0d99f593367d
Egor Smirnov <esmirnov.xyz@gmail.com>
2017-11-24 11:32:07 +0500

    dates mobile is ready

--- src/App.js
+++ src/App.js
@@ -6,6 +6,17 @@ import Header from './Header/';
 import Landing from './MainPage/';
 import Footer from './Footer/';
 import 'flexboxgrid2/flexboxgrid2.css';
+<<<<<<< HEAD
+=======
+import styled from 'styled-components';
+import 'react-dates/lib/css/_datepicker.css';
+
+import Homes from './Homes/';
+
+const Content = styled.div`
+  padding-top: 80px;
+`;
+>>>>>>> 1ce1e02... date mobile is ready
 
 class App extends Component {
   render() {
---
+++ src/Homes/Filters/DatePicker.js
@@ -0,0 +1,171 @@
+import React from 'react';
+import 'react-dates/initialize';
+import { DayPickerRangeController } from 'react-dates';
+import styled from 'styled-components';
+import momentPropTypes from 'react-moment-proptypes';
+import PropTypes from 'prop-types';
+import moment from 'moment';
+import { forbidExtraProps } from 'airbnb-prop-types';
+import { START_DATE, END_DATE, HORIZONTAL_ORIENTATION } from '../constants';
+import omit from 'lodash/omit';
+import './react_dates_overrides.css';
+import arrow from './arrow.svg';
+
+const propTypes = forbidExtraProps({
+  // example props for the demo
+  autoFocusEndDate: PropTypes.bool,
+  initialStartDate: momentPropTypes.momentObj,
+  initialEndDate: momentPropTypes.momentObj,
+
+  keepOpenOnDateSelect: PropTypes.bool,
+  minimumNights: PropTypes.number,
+  isOutsideRange: PropTypes.func,
+  isDayBlocked: PropTypes.func,
+  isDayHighlighted: PropTypes.func,
+
+  // DayPicker props
+  enableOutsideDays: PropTypes.bool,
+  numberOfMonths: PropTypes.number,
+  withPortal: PropTypes.bool,
+  initialVisibleMonth: PropTypes.func,
+  renderCalendarInfo: PropTypes.func,
+
+  navPrev: PropTypes.node,
+  navNext: PropTypes.node,
+
+  onPrevMonthClick: PropTypes.func,
+  onNextMonthClick: PropTypes.func,
+  onOutsideClick: PropTypes.func,
+  renderDay: PropTypes.func,
+
+  // i18n
+  monthFormat: PropTypes.string,
+
+  isRTL: PropTypes.bool
+});
+
+const defaultProps = {
+  // example props for the demo
+  autoFocusEndDate: false,
+  initialStartDate: null,
+  initialEndDate: null,
+
+  // day presentation and interaction related props
+  renderDay: null,
+  minimumNights: 1,
+  isDayBlocked: () => false,
+  isDayHighlighted: () => false,
+  enableOutsideDays: false,
+
+  // calendar presentation and interaction related props
+  withPortal: false,
+  initialVisibleMonth: null,
+  numberOfMonths: 2,
+  onOutsideClick() {},
+  keepOpenOnDateSelect: false,
+  renderCalendarInfo: null,
+  isRTL: false,
+
+  // navigation related props
+  navPrev: null,
+  navNext: null,
+  onPrevMonthClick() {},
+  onNextMonthClick() {},
+
+  // internationalization
+  monthFormat: 'MMMM YYYY'
+};
+
+const DatePickerWrapper = styled.div`
+  height: 100vh;
+  width: 100%;
+`;
+
+const ShowDateContainer = styled.div`
+  margin-left: 8px;
+  margin-bottom: 12px;
+`;
+
+const DateText = styled.span`
+  color: ${props => (props.focused ? '#0F7276' : '#636363')};
+  border-bottom: ${props => (props.focused ? '1px solid #0F7276' : 'none')};
+`;
+
+const Arrow = styled.img`
+  content: url(${arrow});
+  margin-left: 16px;
+  margin-right: 16px;
+`;
+
+class DatePicker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      focusedFirst: true,
+      startDate: props.initialStartDate,
+      endDate: props.initialEndDate,
+      focusedInput: props.autoFocusEndDate ? END_DATE : START_DATE
+    };
+
+    this.onDatesChange = this.onDatesChange.bind(this);
+    this.onFocusChange = this.onFocusChange.bind(this);
+  }
+
+  onDatesChange({ startDate, endDate }) {
+    this.setState({ startDate, endDate });
+  }
+
+  onFocusChange(focusedInput) {
+    this.setState({
+      focusedFirst: !this.state.focusedFirst,
+      focusedInput: !focusedInput ? START_DATE : focusedInput
+    });
+  }
+
+  render() {
+    const { focusedInput, startDate, endDate } = this.state;
+
+    const props = omit(this.props, [
+      'autoFocus',
+      'autoFocusEndDate',
+      'initialStartDate',
+      'initialEndDate'
+    ]);
+
+    const options = {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric'
+    };
+
+    return (
+      <DatePickerWrapper>
+        <ShowDateContainer>
+          <DateText focused={this.state.focusedFirst}>
+            {startDate ? startDate.format('DD-MM-YYYY') : 'Check-in'}
+          </DateText>
+          <Arrow />
+          <DateText focused={!this.state.focusedFirst}>
+            {endDate ? endDate.format('DD-MM-YYYY') : 'Check-out'}
+          </DateText>
+        </ShowDateContainer>
+
+        <DayPickerRangeController
+          {...props}
+          orientation="vertical"
+          verticalHeight="calc(100vh - 100px)"
+          keepOpenOnDateSelect={this.state.keepOpenOnDateSelect}
+          startDate={this.state.startDate}
+          endDate={this.state.endDate}
+          onDatesChange={this.onDatesChange}
+          onFocusChange={this.onFocusChange}
+          focusedInput={focusedInput}
+        />
+      </DatePickerWrapper>
+    );
+  }
+}
+
+export default DatePicker;
